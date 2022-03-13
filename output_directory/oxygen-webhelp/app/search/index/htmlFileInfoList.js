0b4c2f1f3f53c36f56850a431c1b3bd6be88fc4a
aniaklr <92811464+aniaklr@users.noreply.github.com>
2022-03-13 11:07:06 +0100

    wrzucenie plików projo do gita

---
+++ output_directory/oxygen-webhelp/app/search/index/htmlFileInfoList.js
@@ -0,0 +1,3 @@
+define(function () {
+return ["cshelp.html@@@Context Sensitive Help@@@...","topics/c_my_first_concept.html@@@my_first_concept@@@elo...","topics/r_my_first_ref.html@@@my_first_ref@@@...","topics/t_my_first_task.html@@@my_first_task@@@...","topics/top_my_first_topic.html@@@Topik@@@..."];
+});
---
+++ output_directory/oxygen-webhelp/app/search/index/index-3.js
@@ -0,0 +1,4 @@
+define(function () {
+// Auto generated index for searching.
+return {"related":"2*5*8,3*5*8,4*10*2$8","sensitive":"0*20*1","task":"2*0*d,3*30*3$7,4*0*d","topik":"3*0*a,4*30*0$1"};
+});
