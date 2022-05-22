e23429d4e87881034f0dca4ac46afa6dc9521d58
aniaklr <92811464+aniaklr@users.noreply.github.com>
2022-05-22 15:45:45 +0200

    adding my final project

---
+++ pierwsza pomoc przedweterynaryjna/topic/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Akcesoria",p:["p1"]},{w:"Artyku\u0142y",p:["p2"]},{w:"spo\u017Cywcze",p:["p2"]},{w:"Preparaty",p:["p3"]},{w:"apteczne",p:["p3"]},{w:"Apteczka",p:["p4"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4, 5];
+ph["p3"]=[6, 7];
+ph["p4"]=[8];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
